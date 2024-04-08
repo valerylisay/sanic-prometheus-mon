48b205001e668309e4cd20346f1e580fd3842694
ArtyomKozyrev8 <kozirev8@gmail.com>
2024-04-08 21:58:06 +0300

    Fixes. Repaired conversion of http.HTTPStatus to integer in  after_request_handler

---
+++ tests/test_metrics.py
@@ -0,0 +1,17 @@
+import http
+import unittest
+from functools import partial
+
+from sanic_prometheus.metrics import _convert_status_to_int
+
+
+class TestUrlEndpoint(unittest.TestCase):
+    def test__convert_status_to_int(self):
+        assert _convert_status_to_int(200) == 200
+        assert _convert_status_to_int(http.HTTPStatus.OK) == 200
+        assert _convert_status_to_int("200") == 200
+
+        assert _convert_status_to_int("abc") == 0
+        assert _convert_status_to_int(None) == 0
+        assert _convert_status_to_int(["1"]) == 0
+
